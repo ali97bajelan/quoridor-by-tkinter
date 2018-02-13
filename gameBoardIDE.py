53f0d70515ce9bab6754a5435e5d56ddf2deb066
tiredboy97 <36344763+tiredboy97@users.noreply.github.com>
2018-02-14 00:09:12 +0330

    Update gameBoardIDE.py

--- gameBoardIDE.py
+++ gameBoardIDE.py
@@ -7,66 +7,25 @@ import address
 import json
 
 
-def start_gui(session):
+def start_gui(session, num):
     """Starting point when module is the main routine."""
-    global val, w, root
+    global root
     root = Tk()
-    # root.wm_attributes("-transparentcolor", "white")
-    top = New_Toplevel_1(root, session)
-    # _support.init(root, top)
+    top = NewToplevel(root, session, num)
     root.mainloop()
 
 
-w = None
-
-
-def create_New_Toplevel_1(root, *args, **kwargs):
-    '''Starting point when module is imported by another program.'''
-    global w, w_win, rt
-    rt = root
-    w = Toplevel(root)
-    top = New_Toplevel_1(w)
-    # _support.init(w, top, *args, **kwargs)
-    return (w, top)
-
-
-def destroy_New_Toplevel_1():
-    global w
-    w.destroy()
-    w = None
-
-
-class New_Toplevel_1:
-    def __init__(self, top=None, session=None):
-        '''This class configures and populates the toplevel window.
-           top is the toplevel containing window.'''
-        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _fgcolor = '#000000'  # X11 color: 'black'
-        _compcolor = '#d9d9d9'  # X11 color: 'gray85'
-        _ana1color = '#d9d9d9'  # X11 color: 'gray85'
-        _ana2color = '#d9d9d9'  # X11 color: 'gray85'
+class NewToplevel:
+    def __init__(self, top=None, session=None, num=None):
 
         top.geometry("1000x700+200+50")
-        top.title("New Toplevel 1")
+        top.title("Game Board")
         top.configure(background="gray")
-
-        def show(e):
-            print("hi")
-
-        def color(e):  # des, button):
-            print(e.x_root)
-            e.widget.configure(bg="red")
-            self.Button[0][6].configure(bg="blue")
-            a = str(e.widget)
-            print(a[2:])
 
         def music():
             winsound.PlaySound('Vay-Behalash.wav', winsound.SND_ALIAS | winsound.SND_ASYNC)
 
-        # button.configure(background="white")
-        # des.configure(background="red")
-
-        self.Button = [[0 for i in range(9)] for j in range(9)]
+        self.Button = [[0 for i in range(9)] for j in range(9)]  # main buttons
 
         i = 0
         j = 0
@@ -76,7 +35,34 @@ class New_Toplevel_1:
             while j < 9:
                 self.Button[i][j] = Button(top)
                 self.Button[i][j].place(relx=x, rely=y, height=50, width=50)
+                self.Button[i][j].bind("<Button-1>", lambda a=None, f=i, g=j: self.move(session, f, g))
                 self.Button[i][j].configure(background="#d9d9d9")
+
+                j += 1
+                x += 0.08
+            j = 0
+            x = 0.10
+            y += 0.11
+            i += 1
+        ''' make players color '''
+        if num == 4:
+            self.Button[4][0].configure(background="yellow")
+            self.Button[4][8].configure(background="green")
+        self.Button[0][4].configure(background="red")
+        self.Button[8][4].configure(background="blue")
+
+        ''' make horizontal walls'''
+
+        self.h_wall = [[0 for z in range(9)] for w in range(8)]
+        i = 0
+        j = 0
+        x = 0.10
+        y = 0.11
+        while i < 8:
+            while j < 9:
+                self.h_wall[i][j] = Button(top)
+                self.h_wall[i][j].place(relx=x, rely=y, height=15, width=50)
+                self.h_wall[i][j].configure(background="white")
                 j += 1
                 x += 0.08
             j = 0
@@ -84,63 +70,123 @@ class New_Toplevel_1:
             y += 0.11
             i += 1
 
-        self.Button[0][4].bind("<Button-1>", show)
+        ''' make vertical walls'''
 
-        self.Button[0][5].bind("<Button-1>", color)
+        self.v_wall = [[0 for z in range(8)] for w in range(9)]
+        i = 0
+        j = 0
+        x = 0.16
+        y = 0.03
+        while i < 9:
+            while j < 8:
+                self.v_wall[i][j] = Button(top)
+                self.v_wall[i][j].place(relx=x, rely=y, height=50, width=15)
+                self.v_wall[i][j].configure(background="white")
+                j += 1
+                x += 0.08
+            j = 0
+            x = 0.16
+            y += 0.11
+            i += 1
 
-        self.Button[8][4].bind("<Button-1>", lambda event: self.move(session))
+        ''' make center buttons of walls'''
 
-        print(self.Button)
+        self.c_wall = [[0 for z in range(8)] for w in range(8)]
+        i = 0
+        j = 0
+        x = 0.16
+        y = 0.11
+        while i < 8:
+            while j < 8:
+                self.c_wall[i][j] = Button(top)
+                self.c_wall[i][j].place(relx=x, rely=y, height=15, width=15)
+                self.c_wall[i][j].bind("<Button-1>",
+                                       lambda f=None, a=i, b=j: self.decied(self.h_wall[a][b], self.h_wall[a][b + 1],
+                                                                            self.v_wall[a][b], self.v_wall[a + 1][b]))
+                self.c_wall[i][j].configure(background="white")
 
-        self.Button101 = Button(top)
-        self.Button101.place(relx=0.10, rely=0.11, height=15, width=50)
-
-        self.Button102 = Button(top)
-        self.Button102.place(relx=0.18, rely=0.11, height=15, width=50)
-
-        self.Button103 = Button(top)
-        self.Button103.place(relx=0.26, rely=0.11, height=15, width=50)
-
-        self.Button104 = Button(top)
-        self.Button104.place(relx=0.34, rely=0.11, height=15, width=50)
-
-        self.Button105 = Button(top)
-        self.Button105.place(relx=0.42, rely=0.11, height=15, width=50)
-
-        self.Button106 = Button(top)
-        self.Button106.place(relx=0.50, rely=0.11, height=15, width=50)
-
-        self.Button205 = Button(top)
-        self.Button205.place(relx=0.48, rely=0.03, height=50, width=15)
-
-        self.Button215 = Button(top)
-        self.Button215.place(relx=0.48, rely=0.14, height=50, width=15)
-
-        self.Button304 = Button(top)
-        self.Button304.place(relx=0.48, rely=0.11, height=15, width=15)
-        self.Button304.bind("<Button-1>",
-                            lambda event: self.decied(self.Button105, self.Button106, self.Button205, self.Button215))
+                j += 1
+                x += 0.08
+            j = 0
+            x = 0.16
+            y += 0.11
+            i += 1
 
         self.Hayede = Button(top, command=music)
         self.Hayede.place(relx=0.80, rely=0.88, height=75, width=75)
         self.Hayede.configure(text="Play Hayede", background="#d9d9d9")
 
-    def move(self, session):
-        info = json.dumps(self.Button)
-        t = requests.get(address.url['move'], data=info)
-        print(t.content)
-        # print(self.Button[8][4]['bg'],e.widget['rely'])
+    def move(self, session, i, j):
+        # info = json.dumps(self.Button)
+        # t = requests.get(address.url['move'], data=info)
+        # print(t.content)
+        # while
+        x = self.Button[i][j]
+        if i == 0 and j == 0:
+            a = self.Button[1][0]
+            b = self.Button[0][1]
+        elif i == 8 and j == 8:
+            a = self.Button[8][7]
+            b = self.Button[7][8]
+        elif i == 0 and j == 8:
+            a = self.Button[1][8]
+            b = self.Button[0][7]
+        elif i == 8 and j == 0:
+            a = self.Button[8][1]
+            b = self.Button[7][0]
+        elif i == 0 and j != 0:
+            a = self.Button[i][j + 1]
+            b = self.Button[i][j - 1]
+            c = self.Button[i + 1][j]
+        elif j == 0 and i != 0:
+            a = self.Button[i][j + 1]
+            b = self.Button[i - 1][j]
+            c = self.Button[i + 1][j]
+        elif i == 8 and j != 8:
+            a = self.Button[i][j + 1]
+            b = self.Button[i][j - 1]
+            c = self.Button[i - 1][j]
+        elif j == 8 and i != 8:
+            a = self.Button[i][j - 1]
+            b = self.Button[i - 1][j]
+            c = self.Button[i + 1][j]
+        else:
+            a = self.Button[i][j - 1]
+            b = self.Button[i - 1][j]
+            c = self.Button[i + 1][j]
+            d = self.Button[i][j + 1]
+        try:
+            if a['bg'] == 'red':
+                a['bg'] = 'gray88'
+                x['bg'] = 'red'
+                return True
+            if b['bg'] == 'red':
+                b['bg'] = 'gray88'
+                x['bg'] = 'red'
+                return True
+            if c['bg'] == 'red':
+                c['bg'] = 'gray88'
+                x['bg'] = 'red'
+                return True
+            if d['bg'] == 'red':
+                d['bg'] = 'gray88'
+                x['bg'] = 'red'
+                return True
+        except UnboundLocalError:
+            pass
 
     def decied(self, a, b, c, d):
         answer = tkinter.messagebox.askyesnocancel('Which side ?', 'Do you want Horizontal wall?(yes)\n \tVertical(no)')
-        # print(e.widget)
-        print(answer)
         if str(answer) == 'True':
-            a.configure(bg="black")
-            b.configure(bg="black")
+            if a['bg'] == 'black' or b['bg'] == 'black':
+                tkinter.messagebox.showerror('Error', 'You cannot choose this location')
+            else:
+                a.configure(bg="black")
+                b.configure(bg="black")
 
-            print("are")
         if str(answer) == 'False':
-            c.configure(bg="black")
-            d.configure(bg="black")
-            print("Heh")
+            if c['bg'] == 'black' or d['bg'] == 'black':
+                tkinter.messagebox.showerror('Error', 'You cannot choose this location')
+            else:
+                c.configure(bg="black")
+                d.configure(bg="black")
